cfd3cbd897928a6e066c5b799804aca842d58983
Deivid21 <david.parra.ignacio@gmail.com>
2025-02-23 14:27:43 -0300

    nairo: Build MotCamera2 from prebuilt source
    Change-Id: I622585ccae5df98125ad9e68123f186b6a8fac59
    Signed-off-by: Deivid21 <david.parra.ignacio@gmail.com>

--- device.mk
+++ device.mk
@@ -95,6 +95,12 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media/media_codecs_performance_v1.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance.xml \
     $(LOCAL_PATH)/configs/media/media_profiles_vendor.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_vendor.xml
 
+# Moto Camera 2
+TARGET_MOTCAMERA2 := whitney
+TARGET_USES_MOTCAMERA2 := true
+
+$(call inherit-product, vendor/motorola/MotCamera2/motcamera2.mk)
+
 # NFC
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/nfc/libnfc-hal-st.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-hal-st.conf \
